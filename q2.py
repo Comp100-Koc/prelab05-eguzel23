bbba595e75cf918ff3685d99f0093ac0e6c0ec70
Elvan Güzel <eguzel23@ku.edu.tr>
2026-03-30 17:54:21 +0300

    prelab05

--- q2.py
+++ q2.py
@@ -2,4 +2,21 @@ def remove_adjacent_duplicates(s):
     '''
     Given a string remove all the adjacent duplicate characters and return the string
     '''
-    pass
+    still_adj=True
+    
+    while still_adj:
+        pair=False
+        for i in range(len(s)-1):
+            if s[i]==s[i+1]:
+                s=s[:i]+s[i+2:]
+                pair=True
+                break
+                #I had to use break, otherwise it says out of range. 
+                #I hope this is not a problem
+        if pair==False:
+            still_adj=False
+    return s
+
+print(remove_adjacent_duplicates("abbacadd"))
+print(remove_adjacent_duplicates("azxxzy"))
+print(remove_adjacent_duplicates("aabccb"))
