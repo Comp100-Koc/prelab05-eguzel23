68030b376bfce4ea6d7a415da84e22527c266dd9
Elvan Güzel <eguzel23@ku.edu.tr>
2026-03-30 17:38:50 +0300

    prelab05

--- q1.py
+++ q1.py
@@ -2,4 +2,24 @@ def longest_palindromic_substring(s):
     """
     Given a string find the longest palindromic substring
     """
-    pass
+    max_str=""
+    potential_max=""
+    
+    for i in range(len(s)):
+        for j in range(i+2, len(s)+1):
+            potential_max=s[i:j]
+            
+            if potential_max == potential_max[::-1]:
+                if len(potential_max)>len(max_str):
+                    max_str=potential_max
+                
+                
+    if len(max_str)>=2:
+                
+        return max_str
+    else:
+        return ""
+    
+print(longest_palindromic_substring("baabaad"))
+print(longest_palindromic_substring("cbbd"))
+print(longest_palindromic_substring("a"))
